ef34f807e67570912538060405221836d8a33956
IT24101897 <IT24101897@my.sliit.lk>
2024-09-26 21:02:14 +0530

    Add files via upload

---
+++ Q2/IT24101897Lab7Q2A.java
@@ -0,0 +1,23 @@
+public class IT24101897Lab7Q2A {
+  
+  public static void main(String[] args) {
+   
+     for (int i = 1; i <= 4; i++){
+
+          for(int j = 1; j <=5; j++){
+
+             System.out.print("$ ");
+            
+          }
+     
+      System.out.println();
+
+     }
+  
+
+  }
+
+
+}
+
+
