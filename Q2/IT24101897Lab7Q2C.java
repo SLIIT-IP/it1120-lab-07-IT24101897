e1678cc3ff6d8ebbf530751dbaaa03afbb74d5e4
IT24101897 <IT24101897@my.sliit.lk>
2024-09-26 21:03:10 +0530

    Add files via upload

---
+++ Q2/IT24101897Lab7Q2C.java
@@ -0,0 +1,21 @@
+public class IT24101897Lab7Q2C {
+  
+  public static void main(String[] args) {
+
+      for (int rowCount = 5 ; rowCount >= 1; rowCount--) {
+    
+            for(int columnCount = rowCount; columnCount >= 1; columnCount--){
+ 
+               System.out.print(rowCount);
+           
+            }
+   
+           System.out.println();
+   
+       }
+
+   
+    }
+
+
+}
